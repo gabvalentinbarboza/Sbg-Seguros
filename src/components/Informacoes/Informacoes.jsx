e09d779ca1c68f88eb9a5b7772b8aea600d46283
Redd.dev <garrafael27@gmail.com>
2024-10-24 19:19:59 -0300

    upload

--- src/components/Informacoes/Informacoes.jsx
+++ src/components/Informacoes/Informacoes.jsx
@@ -9,10 +9,65 @@ import WhatsAppIcon from '../../assets/WhatsAppIcon.png';
 import InstagramIcon from '../../assets/InstagramIcon.png';
 
 function Informacoes() {
-    return ( 
-        <>
-        </>
-    )
-}
-export default Informacoes;
+    return (
+        <section className={Style.InformacoesContainer}>
 
+            <div className={Style.contactGroup}>
+                <h1 className={Style.contactTitle}>Contato</h1>
+                <ul className={Style.contactList}>
+                    <li className={Style.contactItem}>
+                        <a className={Style.contactLink} href="https://wa.me/5511982040163" target="_blank" rel="noopener noreferrer">
+                            <img src={WhatsAppIcon} alt="WhatsApp" className={Style.contactIcon} />
+                            <span className={Style.contactText}>WhatsApp</span>
+                        </a>
+                    </li>
+                    <li className={Style.contactItem}>
+                        <a className={Style.contactLink} href="tel:113132-1142">
+                            <img src={PhoneIcon} alt="Telefone" className={Style.contactIcon} />
+                            <span className={Style.contactText}>Telefone</span>
+                        </a>
+                    </li>
+                    <li className={Style.contactItem}>
+                        <a className={Style.contactLink} href="mailto:simone.sabbag@sbgseguros.com.br">
+                            <img src={EmailIcon} alt="E-mail" className={Style.contactIcon} />
+                            <span className={Style.contactText}>E-mail</span>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+
+            <div className={Style.socialGroup}>
+                <h1 className={Style.socialTitle}>Siga-nos</h1>
+                <ul className={Style.socialList}>
+                    <li className={Style.socialItem}>
+                        <a className={Style.socialLink} href="https://www.linkedin.com/in/simone-sabbag-30330924" target="_blank" rel="noopener noreferrer">
+                            <img src={LinkedInIcon} alt="LinkedIn" className={Style.socialIcon} />
+                            <span className={Style.socialText}>LinkedIn</span>
+                        </a>
+                    </li>
+                    <li className={Style.socialItem}>
+                        <a className={Style.socialLink} href="https://www.instagram.com/sbg_seguros?igsh=NzRvZTR0Y3l2bHN5" target="_blank" rel="noopener noreferrer">
+                            <img src={InstagramIcon} alt="Instagram" className={Style.socialIcon} />
+                            <span className={Style.socialText}>Instagram</span>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+
+            <div className={Style.officeGroup}>
+                <h1 className={Style.officeTitle}>Escritório</h1>
+                <ul className={Style.officeList}>
+                    <li className={Style.officeItem}>
+                        <a className={Style.officeLink} href="https://maps.app.goo.gl/MzvgmXKc5Vcyr36c6" target="_blank" rel="noopener noreferrer">
+                            <img src={AddressIcon} alt="Endereço" className={Style.officeIcon} />
+                            <span className={Style.officeText}>Rua Bom Sucesso, 220 - Tatuapé, SP</span>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+
+        </section>
+    );
+}
+
+export default Informacoes;
--- src/pages/FaleConosco/FaleConosco.jsx
+++ src/pages/FaleConosco/FaleConosco.jsx
@@ -1,20 +1,27 @@
 import React from "react";
-import { Link } from 'react-dom';
-import Styles from './faleconosco.module.css';
 import Header from "../../components/Header/Header";
-import Footer from '../../components/Footer/Footer';
-import FaleConoscoComponent from '../../components/FaleConoscoComponent/FaleConoscoComponent';
+import Footer from "../../components/Footer/Footer";
+import FaleConoscoComponent from "../../components/FaleConoscoComponent/FaleConoscoComponent";
 import FaleConoscoFundo from "../../components/FaleConoscoFundo/FaleConoscoFundo";
 import Informacoes from "../../components/Informacoes/Informacoes";
+import Style from './faleconosco.module.css'; // Importando o CSS
 
 function FaleConosco() {
     return (
         <>
             <Header />
-            <main>
-                <FaleConoscoFundo />
-                <Informacoes/>
-                <FaleConoscoComponent className={Styles.FaleConoscoComponent} />
+            <main >
+                <FaleConoscoFundo /> {/* Mantido na mesma posição e sem alteração */}
+                <div className={Style.contentContainer}>
+                    <div className={Style.InformacoesContainer}>
+                    <Informacoes />
+                    </div>
+                    
+
+                    <div className={Style.FaleConoscoComponentContainer}>
+                    <FaleConoscoComponent />
+                    </div>
+                </div>
             </main>
             <Footer />
         </>
