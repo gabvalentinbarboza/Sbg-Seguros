3e9f8d27c1d3baff7a1e824b3d9d376842a58b6f
Redd.dev <garrafael27@gmail.com>
2024-10-24 15:04:23 -0300

    Informacoes Adicionadas

---
+++ src/components/Informacoes/Informacoes.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Style from './informacoes.module.css';
+
+import AddressIcon from '../../assets/AddressIcon.png';
+import EmailIcon from '../../assets/EmailIcon.png';
+import LinkedInIcon from '../../assets/LinkedInIcon.png';
+import PhoneIcon from '../../assets/PhoneIcon.png';
+import WhatsAppIcon from '../../assets/WhatsAppIcon.png';
+import InstagramIcon from '../../assets/InstagramIcon.png';
+
+function Informacoes() {
+    return ( 
+        <>
+        </>
+    )
+}
+export default Informacoes;
+
--- src/pages/FaleConosco/FaleConosco.jsx
+++ src/pages/FaleConosco/FaleConosco.jsx
@@ -1,20 +1,22 @@
 import React from "react";
-import {Link} from 'react-dom';
+import { Link } from 'react-dom';
 import Styles from './faleconosco.module.css';
 import Header from "../../components/Header/Header";
 import Footer from '../../components/Footer/Footer';
 import FaleConoscoComponent from '../../components/FaleConoscoComponent/FaleConoscoComponent';
 import FaleConoscoFundo from "../../components/FaleConoscoFundo/FaleConoscoFundo";
+import Informacoes from "../../components/Informacoes/Informacoes";
 
 function FaleConosco() {
-    return(
+    return (
         <>
-        <Header />
-        <main>
-        <FaleConoscoFundo />
-            <FaleConoscoComponent  className={Styles.FaleConoscoComponent}/>
-        </main>
-        <Footer />
+            <Header />
+            <main>
+                <FaleConoscoFundo />
+                <Informacoes/>
+                <FaleConoscoComponent className={Styles.FaleConoscoComponent} />
+            </main>
+            <Footer />
         </>
     );
 }
